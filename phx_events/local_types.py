e6d057ba074051bcfad5946dea93abd5523876e9
Jethro Muller <git@jethromuller.co.za>
2021-08-20 15:13:24 +0200

    Add TYPE_CHECKING guard to local_types

--- phx_events/local_types.py
+++ phx_events/local_types.py
@@ -1,14 +1,16 @@
 from asyncio import Queue, Task
 from logging import Logger
-from typing import Callable, NewType, Optional, TypedDict, Union
+from typing import Callable, NewType, Optional, TYPE_CHECKING, TypedDict, Union
 
-from phx_events.phx_messages import PHXEvent, PHXEventMessage, PHXMessage
+
+if TYPE_CHECKING:
+    from phx_events.phx_messages import PHXEvent, PHXEventMessage, PHXMessage
 
 
 Topic = NewType('Topic', str)
 Event = NewType('Event', str)
-ChannelEvent = Union[PHXEvent, Event]
-ChannelMessage = Union[PHXMessage, PHXEventMessage]
+ChannelEvent = Union['PHXEvent', Event]
+ChannelMessage = Union['PHXMessage', 'PHXEventMessage']
 HandlerFunction = Callable[[ChannelMessage, Logger], None]
 
 
