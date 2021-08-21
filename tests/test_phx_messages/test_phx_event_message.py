eb5f26794a1de72a6d5e030fe19b46575d1d2632
Jethro Muller <git@jethromuller.co.za>
2021-08-21 17:41:57 +0200

    Fix test typing

--- tests/test_phx_messages/test_phx_event_message.py
+++ tests/test_phx_messages/test_phx_event_message.py
@@ -1,13 +1,13 @@
-from phx_events.phx_messages import PHXEvent, PHXEventMessage
+from phx_events.phx_messages import PHXEvent, PHXEventMessage, Topic
 
 
 class TestSubtopic:
     def test_returns_subtopic_if_it_exists(self):
-        phx_message = PHXEventMessage(topic='topic:subtopic:info', ref='ref', event=PHXEvent.reply, payload={})
+        phx_message = PHXEventMessage(topic=Topic('topic:subtopic:info'), ref='ref', event=PHXEvent.reply, payload={})
 
         assert phx_message.subtopic == 'subtopic:info'
 
     def test_returns_none_if_it_does_not_exist(self):
-        phx_message = PHXEventMessage(topic='topic', ref='ref', event=PHXEvent.reply, payload={})
+        phx_message = PHXEventMessage(topic=Topic('topic'), ref='ref', event=PHXEvent.reply, payload={})
 
         assert phx_message.subtopic is None
--- tests/test_phx_messages/test_phx_message.py
+++ tests/test_phx_messages/test_phx_message.py
@@ -1,13 +1,13 @@
-from phx_events.phx_messages import PHXMessage
+from phx_events.phx_messages import Event, PHXMessage, Topic
 
 
 class TestSubtopic:
     def test_returns_subtopic_if_it_exists(self):
-        phx_message = PHXMessage(topic='topic:subtopic:info', ref='ref', event='test_event', payload={})
+        phx_message = PHXMessage(topic=Topic('topic:subtopic:info'), ref='ref', event=Event('test_event'), payload={})
 
         assert phx_message.subtopic == 'subtopic:info'
 
     def test_returns_none_if_it_does_not_exist(self):
-        phx_message = PHXMessage(topic='topic', ref='ref', event='test_event', payload={})
+        phx_message = PHXMessage(topic=Topic('topic'), ref='ref', event=Event('test_event'), payload={})
 
         assert phx_message.subtopic is None
