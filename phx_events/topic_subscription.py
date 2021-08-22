b18d866cdb1f1922454f1ad3d1b31ce52d510c0c
Jethro Muller <git@jethromuller.co.za>
2021-08-22 17:05:09 +0200

    Move registration processing code

--- phx_events/topic_subscription.py
+++ phx_events/topic_subscription.py
@@ -1,13 +1,9 @@
 from asyncio import Event
 from dataclasses import dataclass
 from enum import IntEnum, unique
-from typing import Optional, TYPE_CHECKING
+from typing import Optional
 
 from phx_events.phx_messages import PHXEventMessage
-
-
-if TYPE_CHECKING:
-    from phx_events.channels_client import PHXChannelsClient
 
 
 @unique
@@ -27,13 +23,3 @@ class TopicRegistration:
     connection_ref: str
     status_updated_event: Event
     status: Optional[TopicSubscribeResult] = None
-
-
-async def process_topic_join_reply(message: PHXEventMessage, client: 'PHXChannelsClient') -> None:
-    topic = message.topic
-    client.logger.info(f'Got topic {topic} join reply {message=}')
-
-    status = SubscriptionStatus.SUCCESS if message.payload['status'] == 'ok' else SubscriptionStatus.FAILED
-
-    # Set the topic status map
-    await client.set_topic_join_state(topic, status, message)
