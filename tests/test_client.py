800727552b35827829cefe1beeddd87540982fa8
Jethro Muller <git@jethromuller.co.za>
2021-08-28 15:40:58 +0200

    Add __aenter__ & __aexit__ tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import asyncio
+from unittest.mock import patch
 from urllib.parse import urlencode
 
 import pytest
@@ -44,3 +45,22 @@ class TestPHXChannelsClientInit:
         specified_loop_client = PHXChannelsClient(self.socket_url, event_loop=event_loop)
 
         assert specified_loop_client._loop == event_loop
+
+
+class TestPHXChannelsClientAEnter:
+    async def test_returns_self_when_using_as_with_context_manager(self):
+        phx_client = PHXChannelsClient('ws://web.socket/url/')
+
+        async with phx_client as test_phx_client:
+            assert isinstance(test_phx_client, PHXChannelsClient)
+            assert phx_client == test_phx_client
+
+
+class TestPHXChannelsClientAExit:
+    async def test_calls_shutdown_with_correct_reason(self):
+        phx_client = PHXChannelsClient('ws://web.socket/url/')
+
+        with patch(phx_client.shutdown) as mock_shutdown:
+            await phx_client.__aexit__()
+
+        mock_shutdown.assert_called_with('Leaving PHXChannelsClient context')
