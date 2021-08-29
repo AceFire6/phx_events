7b246c155af5c9aae24393bc9ade10c851342f97
Jethro Muller <git@jethromuller.co.za>
2021-08-29 17:08:12 +0200

    Add proper decimal support - serialise decimal to float and deserialise float to Decimal

--- phx_events/json_handler.py
+++ phx_events/json_handler.py
@@ -4,16 +4,32 @@ from typing import Any, Union
 import orjson
 
 
-def serialiser(_obj: Any) -> str:
+def decimal_serialiser(_obj: Any) -> float:
     if isinstance(_obj, Decimal):
-        return str(_obj)
+        return float(str(_obj))
 
     raise TypeError
 
 
+def deep_float_replace(obj: Any) -> Any:
+    if isinstance(obj, float):
+        return Decimal(str(obj))
+    if isinstance(obj, dict):
+        return {key: deep_float_replace(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [deep_float_replace(value) for value in obj]
+    else:
+        return obj
+
+
 def dumps(obj: Any) -> bytes:
-    return orjson.dumps(obj, default=serialiser)
+    return orjson.dumps(obj, default=decimal_serialiser)
 
 
-def loads(json: Union[bytes, bytearray, memoryview, str]) -> Any:
-    return orjson.loads(json)
+def loads(json: Union[bytes, bytearray, memoryview, str], floats_to_decimal: bool = True) -> Any:
+    parsed_json = orjson.loads(json)
+
+    if not floats_to_decimal:
+        return parsed_json
+
+    return deep_float_replace(parsed_json)
