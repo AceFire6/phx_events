56d426d3ece216cf97ea528022d0393d89c975f9
Jethro Muller <git@jethromuller.co.za>
2021-08-29 19:13:54 +0200

    Add mocked websocket client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,14 @@
 # If this is removed then remove the additional options in
 # the pytest.ini file
+from unittest.mock import patch
+
+import pytest
+
+
 pytest_plugins = ['hypothesis.extra.pytestplugin']
+
+
+@pytest.fixture()
+def mock_websocket_client():
+    with patch('phx_events.client.client', autospec=True) as mocked_websocket:
+        yield mocked_websocket
