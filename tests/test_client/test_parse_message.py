a33d37297669135e8b232300ffd1210bfb236e82
Jethro Muller <git@jethromuller.co.za>
2021-08-29 17:55:36 +0200

    Add parse_message test

--- tests/test_client/test_parse_message.py
+++ tests/test_client/test_parse_message.py
@@ -1,8 +1,27 @@
+from hypothesis import given
 import pytest
+
+from phx_events import json_handler
+from phx_events.client import PHXChannelsClient
+from phx_events.phx_messages import PHXMessage
+from tests.strategy_utils import event_strategy
 
 
 pytestmark = pytest.mark.asyncio
 
 
 class TestPHXChannelsClientParseMessage:
-    pass
+    @given(event_strategy())
+    def test_returns_phx_event_message_for_event_socket_message(self, event_dict):
+        phx_client = PHXChannelsClient('ws://url')
+        socket_message = json_handler.dumps(event_dict)
+        # This is done to convert any Python types in the event_dict payload to JSON-versions
+        reserialised_event_dict = json_handler.loads(socket_message)
+
+        parsed_message = phx_client._parse_message(socket_message)
+
+        assert isinstance(parsed_message, PHXMessage)
+        assert parsed_message.topic == reserialised_event_dict['topic'] == event_dict['topic']
+        assert parsed_message.event == reserialised_event_dict['event'] == event_dict['event']
+        assert parsed_message.ref == reserialised_event_dict['ref'] == event_dict['ref']
+        assert parsed_message.payload == (reserialised_event_dict['payload'] or {})
